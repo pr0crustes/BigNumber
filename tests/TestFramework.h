7e45f2accad52b69fb4bfb2c068e8531168d66e1
pr0crustes <dark.pr0crustes@gmail.com>
2018-11-22 19:21:32 -0200

    Tf_TIME_iT now times it in nanoseconds.

--- tests/TestFramework.h
+++ tests/TestFramework.h
@@ -143,9 +143,9 @@ namespace _TF_IGNORE {  // Namespace for hidding stuff.
 	std::chrono::high_resolution_clock::time_point to = std::chrono::high_resolution_clock::now(); \
 	{ code ; } \
 	std::chrono::high_resolution_clock::time_point tf = std::chrono::high_resolution_clock::now(); \
-	double duration = std::chrono::duration_cast<std::chrono::microseconds>(tf - to).count(); \
-	double asSeconds = duration / 1000000; \
-	std::cout << "Execution of test at file \'" << __FILE__ "\' at line \'" << __LINE__ << "\' took: " << duration << " micro seconds, or " << asSeconds << " seconds." << std::endl; \
+	double duration = std::chrono::duration_cast<std::chrono::nanoseconds>(tf - to).count(); \
+	double asSeconds = duration / 1000000000; \
+	std::cout << "Execution of test at file \'" << __FILE__ "\' at line \'" << __LINE__ << "\' took: " << duration << " nanoseconds, or " << asSeconds << " seconds." << std::endl; \
 }
 
 
